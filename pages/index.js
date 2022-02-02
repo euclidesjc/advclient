7bc5db1b5516de5c4a8b9aaaa4bb31f9922d6166
euclidesjc <euclides_jc@hotmail.com>
2022-02-02 02:37:23 -0300

    pagina sobre

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 function Home(){
 
-    return <div>Pagina Principal</div>
+    return <div>
+        <h1>Página Principal</h1>
+        <a href="/sobre"> Página Sobre</a>
+        </div>
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,9 @@
+function Sobre(){
+
+    return <div>
+        <h1>Página Sobre</h1>
+        <a href="/"> Página Principal</a>
+        </div>
+}
+
+export default Sobre
